53563330e2c4d7d1debd97444285350a7f8e2148
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-22 17:19:16 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const urlRoute = require('./routes/url')
+const path = require('path')
 const URL = require('./models/url')
+
 const { connectMongoDb } = require('./connection')
 
 const app = express()
@@ -10,10 +12,20 @@ connectMongoDb('mongodb://127.0.0.1:27017/shortner').then(() => {
   console.log('MongoDb Connected')
 })
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
 app.use(express.json())
 
-app.get("/", (req, res)=>{
-  return res.json({msg: "Hello from Shortner"})
+app.get('/test', async (req, res) => {
+  const allUrls = await URL.find({})
+  return res.render('home', {
+    urls: allUrls,
+  })
+})
+
+app.get('/', (req, res) => {
+  return res.json({ msg: 'Hello from Shortner' })
 })
 
 app.use('/url', urlRoute)
