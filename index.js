3dd577bec1f3d9ea3de0d472209d13ee4fbaa933
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-23 18:53:51 +0530

    added userRoute

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const urlRoute = require('./routes/url')
 const path = require('path')
-const staticRoute = require('./routes/staticRouter')
-
 const { connectMongoDb } = require('./connection')
+
+const staticRoute = require('./routes/staticRouter')
+const urlRoute = require('./routes/url')
+const userRoute = require('./routes/user')
 
 const app = express()
 const PORT = 5001
@@ -16,10 +17,11 @@ app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/url', urlRoute)
 app.use('/', staticRoute)
+app.use('./user', userRoute)
 app.listen(PORT, () => {
   console.log(`Server started at PORT:${PORT}`)
 })
