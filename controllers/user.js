03e6791da34871dcacf3ce61f13bb160ec04c597
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-24 00:47:00 +0530

    added login auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,34 @@
 const User = require('../models/user')
 async function handleUserSignup(req, res) {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
 
   await User.create({
     name,
     email,
     password,
-  });
+  })
   return res.render('home')
 }
 
-module.exports = { handleUserSignup }
+async function handleUserSignup(req, res) {
+  const { name, email, password } = req.body
+
+  await User.create({
+    name,
+    email,
+    password,
+  })
+  return res.redirect('/')
+}
+
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({email, password})
+  if(!user){
+    res.render('login', {error: "Invalid Username or Password"});
+  }
+  return res.redirect('/')
+}
+
+module.exports = { handleUserSignup, handleUserLogin }
