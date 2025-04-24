0e3636d06cf2940ac2848ad155683565cd3e2d49
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-24 16:47:43 +0530

    done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const { getUser } = require('../service/auth')
 
 async function restrictToLoggedInUserOnly(req, res, next) {
-  const userUid = req.cookies.uid
+  const userUid = req.cookies?.uid
 
   if (!userUid) return res.redirect('/login')
   const user = getUser(userUid)
