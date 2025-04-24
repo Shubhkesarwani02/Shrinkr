14b1ec40774559471657e91abc20570bfc2ab840
Shubhkesarwani02 <122770084+Shubhkesarwani02@users.noreply.github.com>
2025-04-25 00:07:13 +0530

    updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/url', urlRoute)//login to homepage infinite loop - bug
+app.use('/url', restrictToLoggedInUserOnly, urlRoute)
 app.use('/user', userRoute)
 app.use('/', staticRoute)
 app.listen(PORT, () => {
